58eec02e37305bed362c331b79c5976d0224462f
Vihangi Sandupama <vihangisandupama@gmail.com>
2025-09-15 22:50:12 +0530

    Vihangi

--- frontend/src/components/Inventory/inventory.js
+++ frontend/src/components/Inventory/inventory.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import Navbar from '../NavBar/NavBar'
+import React, {useEffect, useState} from 'react';
+
+import Navbar from '../NavBar/NavBar';
 import axios from "axios";
+import InventoryItem from '../Inventory/Inventory';
+
+const URL = "http://localhost:5000/Inventory";
+
+const fetchHandler = async () =>{
+  return await axios.get(URL).then((res) => res.data);
+}
 
 function Inventory() {
+
+  const [Inventory, setInventory] = useState([]);
+  useEffect(()=> {
+    fetchHandler().then((data) => setInventory(data.Inventory));
+  },[])
+
   return (
     <div>
       <Navbar/>
+      <div>
+        {Inventory && Inventory.map ((items, i) => (
+          <div key= {i}>
+            <InventoryItem items={items}/>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
